cdf168aa4f652f35dbe9c6930661f30dbcad2df5
simsta1 <simon.a.staehli@gmail.com>
2025-05-19 22:36:11 +0200

    feat: change string

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2025 DevOps Course! Cloud Deploy auf Render.com. This is a Test-Commit. Cloud Deployment auf Render.com');
+  res.send('DevOps-Course - Azure Deployment via Github Action');
 });
 
 app.listen(PORT, HOST);
